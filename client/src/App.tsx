e36e2bf18905fcf1107e16d0de44caaef2a9891a
ulrikson <erik.ulrikson@gmail.com>
2025-02-09 11:55:31 +0100

    slate 900

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
 
 	return (
 		// todo: refactor to components
-		<div className="pt-8 min-h-screen bg-slate-950 text-white">
+		<div className="pt-8 min-h-screen bg-slate-900 text-white">
 			<div className="max-w-7xl mx-auto px-4 py-8">
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-8">
 					<Card>
